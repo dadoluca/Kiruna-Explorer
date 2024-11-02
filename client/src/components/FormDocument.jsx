8afb3499affa943a11bd4d134090b8d0dee2a014
giacomobelluardo <giacomino21@icloud.com>
2024-11-02 14:46:31 +0100

    Map UI, Card visualization in the Map and Form

--- client/src/components/FormDocument.jsx
+++ client/src/components/FormDocument.jsx
@@ -7,14 +7,15 @@ import { Button, Row, Col, Card } from 'react-bootstrap';
 function DocumentInsert() {
     const navigate = useNavigate();
 
+    const [errors, setErrors] = useState({});
     const [title, setTitle] = useState('');
     const [stakeholders, setStakeholders] = useState('');
     const [type, setType] = useState('');
     const [scale, setScale] = useState('');
-    const [date, setDate] = useState('');
+    const [date, setDate] = useState(Date.now());
     const [connections, setConnections] = useState(0);
-    const [pages, setPages] = useState('');
-    const [language, setLanguage] = useState('');
+    const [pages, setPages] = useState('Not specified');
+    const [language, setLanguage] = useState('Not specified');
     const [longitude, setLongitude] = useState(0.0);
     const [latitude, setLatitude] = useState(0.0);
 
@@ -25,13 +26,39 @@ function DocumentInsert() {
         setStakeholdersArray(ev.target.value.split(','));
     }
 
+    const validateForm = () => {
+        let newErrors = {};
+        
+        if (!title) newErrors.title = 'Title is required';
+        if (!stakeholders) newErrors.stakeholders = 'Stakeholders are required';
+        if (!type) newErrors.type = 'Type is required';
+        if (!scale) newErrors.scale = 'Scale is required';
+        if (!latitude) newErrors.latitude = 'Latitude is required';
+        if (!longitude) newErrors.longitude = 'Longitude is required';
+        if (connections <= 0) newErrors.connections = 'Connections must be at least 1';
+    
+        setErrors(newErrors);
+    
+        // Return true if there are no errors, false otherwise
+        return Object.keys(newErrors).length === 0;
+    };
+    
+
     const handleSubmit = async()=>{
+        if (!validateForm()) {
+            console.error('Validation failed');
+            return;
+        }
+
+        onSubmit({ title, scale, date, stakeholdersArray, type, connections, pages, language, latitude, longitude });
+
         const document = {
             title: title,
             stakeholders: stakeholdersArray,
             type: type,
             scale: scale,
             date: date,
+            language: language,
             connections: connections,
             pages: pages,
         }
@@ -60,12 +87,11 @@ function DocumentInsert() {
     const handleReturnHome = () => {
         navigate('/');
     }
-    
 
     return (
         <>
         <Card>
-            <Card.Title>Insert document</Card.Title>
+            <Card.Title>INSERT DOCUMENT</Card.Title>
             <Card.Body>
                 <FloatingLabel
                 label="Title of the document"
@@ -73,7 +99,11 @@ function DocumentInsert() {
             >
                 <Form.Control type="text" value={title}               
                     onChange={(ev) => setTitle(ev.target.value)}
+                    isInvalid={!!errors.title}
                     required={true}/>
+                <Form.Control.Feedback type="invalid">
+                    {errors.title}
+                </Form.Control.Feedback>
             </FloatingLabel>
             <FloatingLabel
                 label="Stakeholders"
@@ -81,7 +111,11 @@ function DocumentInsert() {
             >
                 <Form.Control type="text"  value={stakeholders}
                     onChange={(ev) => handlestakeholders(ev)}
+                    isInvalid={!!errors.stakeholders}
                     required={true}/>
+                <Form.Control.Feedback type="invalid">
+                    {errors.stakeholders}
+                </Form.Control.Feedback>
             </FloatingLabel>
             <FloatingLabel
                 label="Scale"
@@ -89,16 +123,18 @@ function DocumentInsert() {
             >
                 <Form.Control type="text" value={scale}
                     onChange={(ev) => setScale(ev.target.value)}
+                    isInvalid={!!errors.scale}
                     required={true}/>
+                <Form.Control.Feedback type="invalid">
+                    {errors.scale}
+                </Form.Control.Feedback>
             </FloatingLabel>
             <FloatingLabel
                 label="Issuance date"
                 className="mb-3"
-                
             >
                 <Form.Control type="date" value={date}
-                    onChange={(ev) => setDate(ev.target.value)}
-                    required={true}/>
+                    onChange={(ev) => setDate(ev.target.value)}/>
             </FloatingLabel>
             <FloatingLabel
                 label="Type of the document"
@@ -106,7 +142,11 @@ function DocumentInsert() {
             >
                 <Form.Control type="text" value={type}
                     onChange={(ev) => setType(ev.target.value)}
+                    isInvalid={!!errors.type}
                     required={true}/>
+                <Form.Control.Feedback type="invalid">
+                    {errors.type}
+                </Form.Control.Feedback>
             </FloatingLabel>
             <FloatingLabel
                 label="Connections"
@@ -114,23 +154,25 @@ function DocumentInsert() {
             >
                 <Form.Control type="number" value={connections}
                     onChange={(ev) => setConnections(ev.target.value)}
+                    isInvalid={!!errors.connections}
                     required={true}/>
+                <Form.Control.Feedback type="invalid">
+                    {errors.connections}
+                </Form.Control.Feedback>
             </FloatingLabel>
             <FloatingLabel
                 label="Language"
                 className="mb-3"
             >
                 <Form.Control type="text" value={language}
-                    onChange={(ev) => setLanguage(ev.target.value)}
-                    required={true}/>
+                    onChange={(ev) => setLanguage(ev.target.value)}/>
             </FloatingLabel>
             <FloatingLabel
                 label="Pages"
                 className="mb-3"
             >
                 <Form.Control type="text" value={pages}
-                    onChange={(ev) => setPages(ev.target.value)}
-                    required={true}/>
+                    onChange={(ev) => setPages(ev.target.value)}/>
             </FloatingLabel>
             <Row className="align-items-end">
                 <Col md={6}>
@@ -140,7 +182,11 @@ function DocumentInsert() {
                         >
                             <Form.Control type="number" value={latitude}
                                 onChange={(ev) => setLatitude(ev.target.value)}
+                                isInvalid={!!errors.latitude}
                                 required={true}/>
+                            <Form.Control.Feedback type="invalid">
+                                {errors.latitude}
+                            </Form.Control.Feedback>
                         </FloatingLabel>
                 </Col>
                 <Col md={6}>
@@ -150,7 +196,11 @@ function DocumentInsert() {
                     >
                         <Form.Control type="number" value={longitude}
                             onChange={(ev) => setLongitude(ev.target.value)}
+                            isInvalid={!!errors.longitude}
                             required={true}/>
+                        <Form.Control.Feedback type="invalid">
+                            {errors.longitude}
+                        </Form.Control.Feedback>
                     </FloatingLabel>
                 </Col>
             </Row>
@@ -158,12 +208,10 @@ function DocumentInsert() {
             <div className="buttons">
                 <Row>
                     <Col>
-                        <Button className="mt-3" type="submit" onClick={handleSubmit}>Add</Button>
+                        <Button className="mt-3" variant="dark" onClick={handleSubmit}>Add</Button>
                     </Col>
-                </Row>
-                <Row>
                     <Col>
-                        <Button className="mt-3" variant="danger" onClick={handleReturnHome}>Back</Button>
+                        <Button className="mt-3" variant="dark" onClick={handleReturnHome}>Back</Button>
                     </Col>
                 </Row>
             </div>
--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,16 +1,70 @@
 import React from 'react';
+import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import DetailPlanCard from './CardDocument';
+import { useNavigate } from 'react-router-dom';
+import { Button  } from 'react-bootstrap';
 
 const MapComponent = () => {
+    const navigate = useNavigate();
+
     const position = [67.8558, 20.2253]; //Kiruna coordinates
+    const [showForm, setShowForm] = useState(false);  //forrm flag
+    const [markers, setMarkers] = useState([]);       //marker array, each marker is referred to a document
+    const [selectedMarker, setSelectedMarker] = useState(null); 
+
+    //retrieve markers from Backend API 
+    //TO ADD
+
+    //Function for managing button click
+    const handleButtonClick = () => {
+        navigate('/document-creation');
+    };
+
+    /*
+    //Function for managing form submission (used for static frontend testing)
+    const handleFormSubmit = ({ title, scale, date, stakeholdersArray, type, connections, pages, language, latitude, longitude }) => {
+        const newMarker = { 
+            index: markers.length,
+            title: title,
+            scale: scale,
+            date: date,
+            stakeholders: stakeholdersArray,
+            type: type,
+            connections: connections,
+            pages: pages,
+            language: language,
+            latitude: parseFloat(latitude), 
+            longitude: parseFloat(longitude) };
+        console.log(newMarker);
+        setMarkers([...markers, newMarker]);    //add new marker to the array of markers(graphic rappresentation of the documents)                
+    };
+    */
+    
+    //Function for managing marker click
+    const handleMarkerClick = (marker) => {
+        setSelectedMarker(marker);
+    };
 
     return (
-        <MapContainer center={position} zoom={13} style={{ height: '500px', width: '100%' }}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-        </MapContainer>
+        <div className="mapCont">
+            <Button variant="dark" onClick={handleButtonClick}>Add Document</Button>
+            <MapContainer center={position} zoom={13} style={{ height: '590px', width: '100%' }}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+
+            {markers.map((marker, index) => (
+                <Marker key={index} position={[marker.latitude, marker.longitude]} eventHandlers={{ click: () => handleMarkerClick(marker) }}>
+                    <Popup>
+                    <DetailPlanCard doc={selectedMarker} onClose={() => setSelectedMarker(null)} />
+                    </Popup>
+                </Marker>
+                ))
+            }
+            </MapContainer>
+        </div>
     );
 };
 
