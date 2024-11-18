77e2de30f54daa94d5034df3338bdde4379570ff
giacomobelluardo <giacomino21@icloud.com>
2024-11-18 09:31:25 +0100

    list visualization works

--- client/src/components/ListDocument.jsx
+++ client/src/components/ListDocument.jsx
@@ -1,23 +1,41 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from './ListDocument.module.css'; 
-import CardDoc from "./CardDocVisualization";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import PropTypes from "prop-types";
 
-const ScrollableDocumentsList = ({markers}) => {
-    console.log(markers);
+const ScrollableDocumentsList = (props) => {
+    const markers = props.markers;
+
+    const handleVisualization = (title, description) => {
+        props.handleVisualization(title, description);
+    }
 
     return (
         <div className={styles.scrollableCardList}>
             <h4 className={styles.scrollbarTitle}>Document Types</h4>
-            {markers.map((doc, index) => (
-                <CardDoc
-                    key={index}
-                    title={doc.title}
-                    description={doc.description}
-                />
+            {markers.map((marker, index) => (
+                    <Card >
+                    <Card.Img variant="top" src={marker.icon} />
+                    <Card.Header>{marker.type}</Card.Header>
+                    <Card.Body>
+                      <Card.Title>{marker.title}</Card.Title>
+                      <Card.Text>
+                        {marker.description}
+                      </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                      <Button variant="dark" onClick={() => handleVisualization(marker.title, marker.description)}>Visualize document <i className="bi bi-arrows-angle-expand"></i></Button>
+                    </Card.Footer>
+                  </Card>
             ))}
         </div>
     );
+};
+
+ScrollableDocumentsList.propTypes = {
+    markers: PropTypes.array,
 };
 
 export default ScrollableDocumentsList;
