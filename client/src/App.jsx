2851592ef6d9eff87d854fd80a335a9782aed2d7
Luca Dadone <luca.dadone01@gmail.com>
2024-11-24 05:37:12 +0100

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,6 @@ import MapPage from './pages/MapPage';
 import LoginPage  from './pages//LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DocumentCreationPage from './pages/DocumentCreationPage';
-import ResourceCreationPage from './pages/ResourceCreationge';
 import ScrollableDocumentsList from './components/ListDocument';
 
 function App() {
