f7898cdea618bd88b154c6d53d2699d2b2264d1e
Luca Dadone <luca.dadone01@gmail.com>
2024-11-28 11:43:04 +0100

    get All Documents fixed

--- server/controllers/documentController.mjs
+++ server/controllers/documentController.mjs
@@ -42,7 +42,7 @@ export const createDocument = async (req, res) => {
 export const getAllDocuments = async (req, res) => {
   try {
     // Pagination and filter parameters
-    const { page = 1, limit = 10, title, type, tag } = req.query;
+    const { page = 1, limit = 100, title, type, tag } = req.query;
 
     // Construct filter object based on query parameters
     let filter = {};
