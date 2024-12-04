af2e268946afdeb6d32c6ce24f189efc8d14b6c3
Luca Dadone <luca.dadone01@gmail.com>
2024-12-04 12:44:59 +0100

    bigger close mode selection button

--- client/src/components/CloseModeSelectionButton.jsx
+++ client/src/components/CloseModeSelectionButton.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import styles from './map.module.css'; 
 
 const CloseModeSelectionButton= ({ onClick }) => {
     return (
         <button
-            style={{ backgroundColor: 'transparent', color: 'white', border: 'none' }}
+            style={{ height: '100%',  backgroundColor: 'transparent', color: 'white', border: 'none' }}
             onClick={onClick}
         >
             <FontAwesomeIcon icon={faTimes} />
