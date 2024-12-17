7bfacfb623c0b596320dca30382a59dd5a7dba97
giacomobelluardo <giacomino21@icloud.com>
2024-12-17 21:09:04 +0100

    adjusted the button for selecting more documents

--- client/src/components/SelectDocumentsButton.jsx
+++ client/src/components/SelectDocumentsButton.jsx
@@ -1,48 +1,51 @@
-import React, { useState, useContext } from 'react';
-import { useMapEvents } from 'react-leaflet';
-import { AuthContext } from '../contexts/AuthContext';
+import React, { useContext } from 'react';
 import { DocumentContext } from '../contexts/DocumentContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faCirclePlus, faHouseChimney, faMapMarker, faPlus, faSquarePlus } from '@fortawesome/free-solid-svg-icons';
+import { faCheckDouble, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
 import styles from './Map.module.css';
 import styl from './SelectDocumentsButton.module.css';
-import { SelectionState } from './SelectionState';
 import CloseModeSelectionButton from './CloseModeSelectionButton';
-import PropTypes from 'prop-types';
 
 const SelectDocumentsButton = () => {
     const { selectedDocs, setSelectedDocs, selectingMode, setSelectingMode } = useContext(DocumentContext);
 
     return (
         <>
+        {!selectingMode &&
             <button
                 className={`${styles.selectDocumentsButton}`}
                 onClick={() => {
                                 setSelectingMode(prev => !prev);
                                 console.log(selectedDocs);
                             }}
-        >
-                            {
-                                !selectingMode && <i class="bi bi-files"></i>
-                            }
-                            {
-                                selectingMode && <i class="bi bi-send"></i>
-                            }
+            >
+                <FontAwesomeIcon icon={faLayerGroup} />
             </button>
-                
+        }
 
-                    {
-                        selectingMode &&
-                        <button
-                        className={`${styles.selectDocumentsButton}`}
-                        onClick={() => {
-                            setSelectedDocs([]);
-                            setSelectingMode(prev => !prev);
-                        }}
-                         >
-                            <i class="bi bi-x"></i>
-                        </button>
-                    }
+        { 
+            selectingMode && (
+                        <div className={styl.verticalAlignment}>
+                            <div style={{ height: '55px' }}>
+                                <p>Confirm selections {" "}
+                                    <button className={styl.buttonLink} onClick={() => {console.log("TO IMPLEMENT VISUALIZATION OF THE AREA OF SELECTED DOCS: ", selectedDocs)}}>     
+                                        <FontAwesomeIcon icon={faCheckDouble} />
+                                    </button>
+                                </p>
+                            </div>
+                            {/* Close button - always rendered */}
+                            <div>
+                                <p>
+                                    Back {" "}
+                            <CloseModeSelectionButton
+                                isVisible={selectingMode}
+                                onClick={() => { setSelectedDocs([]); setSelectingMode(prev => !prev); }}
+                            />
+                                </p>
+                            </div>
+                        </div>
+            )
+        }
         </>
     );
 }
