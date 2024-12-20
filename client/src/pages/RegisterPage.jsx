1b916c483863213025e847c46ba893bb5384c3d4
Mohammad Khataei <101052242+Mohammad-khataeii@users.noreply.github.com>
2024-12-20 17:18:04 +0100

    Update RegisterPage.jsx

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,10 +6,13 @@ import RegisterForm from '../components/RegisterForm';
 function RegisterPage() {
   return (
     <Container className={styles.registerPage}>
-      <Col md="12"> 
+      <Col md="12">
         <Card className={styles.registerCard}>
           <Card.Body>
-            <Card.Title className={styles.title}>Urban Planner Registration</Card.Title>
+            <Card.Title className={styles.title}>User Registration</Card.Title>
+            <p className={styles.subtitle}>
+              Select your role to proceed with the registration.
+            </p>
             <RegisterForm />
           </Card.Body>
         </Card>
