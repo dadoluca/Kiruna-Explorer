d2e16dd0a67a07a7cc8823fa4f0a72abcb6d204d
Giada Socorro s334048 <s334048@studenti.polito.it>
2024-12-21 12:29:00 +0100

    Updated Docker config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { 
+    host: process.env.DOCKER ? '0.0.0.0' : 'localhost',
+    port: 5173, 
+  },
   plugins: [react()],
   build: {
     outDir: 'build',
